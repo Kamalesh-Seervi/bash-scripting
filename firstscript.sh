1e2cbcc15117177585380bbafee4a9511888096f
Kamalesh-Seervi <kkamalesh117@gmail.com>
2023-04-21 12:06:44 +0530

    printing system task manager

---
+++ firstscript.sh
@@ -0,0 +1,11 @@
+#/!bin/bash
+
+### this script prints the system info:
+echo "welcome to bash scripting"
+echo "==============================="
+echo "uptime"
+uptime
+echo "================================"
+echo "Task manager"
+htop
+echo "================================"
