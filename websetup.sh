ca3fcf1240e9f2c3fad530032442b8eae95d8fcf
Kamalesh-Seervi <kkamalesh117@gmail.com>
2023-04-21 12:38:16 +0530

    websetup_httpd / centos vm

---
+++ websetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# install dependancies
+sudo yum install wget unzip httpd -y
+
+#start and enable
+
+sudo systemctl start httpd
+sudo system enable httpd
+
+mkdir - p /tmp/webfiles
+cd /tmp/webfiles
+
+wget https://www.tooplate.com/zip-templates/2098_health.zip
+unzip 2098_health.zip
+cp -r 2098_health.zip/* /var/www/html/
+
+#clean up 
+systemctl restart httpd
+rm -rf /tmp/webfiles
